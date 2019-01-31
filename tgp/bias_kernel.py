86b0d99c10b90892c3611b232476222275d61bc0
Martin Ingram <martin.ingram@gmail.com>
2019-01-31 02:57:15 +0000

    Added bias kernel

---
+++ tgp/bias_kernel.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tgp.kernel import Kernel
+
+
+class BiasKernel(Kernel):
+
+    def __init__(self, sd):
+
+        self.sd = sd
+
+    def calculate(self, X1, X2):
+
+        shape = (X1.shape[0], X2.shape[0])
+        result = np.zeros(shape)
+
+        return result + (self.sd ** 2)
