f2781c3b067c42aac0bd6a77d90f918a0f7daadd
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-02-18 12:56:48 +1100

    Removed jupyter notebooks that are not relevant to submission

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ from setuptools import find_packages
 setup(
     name='tennis-gp',
     version=getenv("VERSION", "LOCAL"),
-    description='Predicts tennis matches using a Gaussian Process',
+    description='Predicts matches using a Gaussian Process',
     packages=find_packages()
 )
